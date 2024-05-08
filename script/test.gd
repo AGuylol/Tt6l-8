d9903c6870031ad779d6297cec5812aec67b7cea
Amir Hakimi <selamat.sejahtera.148@gmail.com>
2024-05-08 23:48:56 +0800

    better movement / working bow

--- script/test.gd
+++ script/test.gd
@@ -1,34 +1,53 @@
 extends CharacterBody2D
 
-var max_speed = 100
-var accel = 1000
-var friction = 800
+const accel = 800
+const friction = 400
+const max_speed = 100
 
-var input = Vector2.ZERO
+enum {idle , walk}
+var state = idle
+
+@onready var animationtree = $AnimationTree
+@onready var state_machine = animationtree["parameters/playback"]
+
+var blend_position : Vector2 = Vector2.ZERO
+var blend_pos_paths = [
+	"parameters/idle/idle_bs2d/blend_position",
+	"parameters/walk/walk_bs2d/blend_position"
+]
+var animtree_state_keys = [
+	"idle",
+	"walk"
+]
 
 func _physics_process(delta):
-	player_movement(delta)
-
-func _get_input():
-	input.x= int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	input.y= int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	return input.normalized()
+	move(delta)
+	movement_anim()
+	look_at(get_global_mouse_position())
 	
 	
-func player_movement(delta):
-	input = _get_input()
-	if input == Vector2.ZERO:
-		if velocity.length() > (friction * delta):
-			velocity -= velocity.normalized() * (friction * delta)
-			
-		else:
-			velocity = Vector2.ZERO
-			
+func move(delta):
+	var input_vector = Input.get_vector("left" , "right" , "up" , "down")
+	if input_vector == Vector2.ZERO:
+		state = idle
+		apply_friction(friction * delta)
 	else:
-		velocity += (input * accel * delta)
-		velocity = velocity.limit_length(max_speed)
+		state = walk
+		apply_movement(input_vector * accel * delta)
+		blend_position = input_vector
 		
 	move_and_slide()
 	
-	
-	
+func apply_friction(amount) -> void:
+	if velocity.length() > amount:
+		velocity -= velocity.normalized() * amount
+	else:
+		velocity = Vector2.ZERO
+		
+func apply_movement(amount) -> void:
+	velocity += amount
+	velocity = velocity.limit_length(max_speed)
+
+func movement_anim() -> void:
+	state_machine.travel(animtree_state_keys[state])
+	animationtree.set(blend_pos_paths[state] , blend_position)
