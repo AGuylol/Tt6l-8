08aa5250edd1ad0744437caf65db353949018e1d
Amir Hakimi <selamat.sejahtera.148@gmail.com>
2024-05-15 02:29:09 +0800

    attack animation/collision follow player direction

--- script/arrow.gd
+++ script/arrow.gd
@@ -15,5 +15,4 @@ func _on_visible_on_screen_enabler_2d_screen_exited():
 
 	queue_free()
 	
-func arrow_deal_damage():
-	pass
+
--- script/enemytest.gd
+++ script/enemytest.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+signal enemy_attack(player)
 var speed = 35
 var player_chase = false
 var player = null
@@ -13,20 +14,11 @@ func _physics_process(delta):
 	movement()
 	slime_damaged()
 	
+	
 func movement():
 	if player_chase:
 		position += (player.position - position) / speed
 
-func _on_hitbox_body_entered(body):
-	if body.has_method("player"):
-		player_attacked = true
-		
-	
-
-func _on_hitbox_body_exited(body):
-	if body.has_method("player"):
-		player_attacked = false
-		
 
 func slime_damaged():
 	if player_attacked == true:
@@ -36,32 +28,36 @@ func slime_damaged():
 			self.queue_free()
 
 func _on_detection_body_entered(body):
-	player = body
-	player_chase = true
+	if not body.is_in_group("enemy"):
+		player = body
+		player_chase = true
 
 func _on_detection_body_exited(body):
 	player = null
 	player_chase = false
 
-func enemy():
-	pass
 
-func _on_timer_timeout():
-	$Timer.stop()
-	player_attacked = false
-	
-	
 func _on_hitbox_area_entered(area):
 	var damage
-	if area.has_method("arrow_deal_damage"):
+	if area.is_in_group("arrow"):
 		damage = 50
 		take_damage(damage)
 		
+	elif area.is_in_group("sword"):
+		print("worked")
+		damage = 20
+		take_damage(damage)
+		
 func take_damage(damage):
+	
 	health -= damage
 	print(health)
 	if health <= 0:
 		$AnimatedSprite2D.play("death")
 		await get_tree().create_timer(0.8).timeout
 		queue_free()
+		
+func attack_player():
+	if player:
+		emit_signal("enemy_attack", player)
 	
