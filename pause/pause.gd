99f209caef2c3ac4ed601288c0ddaece578de174
Hakimi <ammarbaharun@gmail.com>
2024-05-16 15:50:25 +0800

    add pause & deathscreen

--- pause/pause.gd
+++ pause/pause.gd
@@ -1,16 +1,28 @@
 extends Control
 
-var pause
+var stop
 var option
 var video
 var audio
 
+func resume():
+	get_tree().paused = false
+
+func pause():
+	get_tree().paused = true
+
+func testEsc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:\
+		resume()
+
 
 func _ready():
-	pause = $Pause
-	option = $Option
-	video = $Video
-	audio = $Audio
+	stop = $PanelContainer/Stop
+	option = $PanelContainer/Option
+	video = $PanelContainer/Video
+	audio = $PanelContainer/Audio
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -26,12 +38,8 @@ func show_and_hide(first,second):
 	second.hide()
 
 
-func _on_continue_pressed():
-	pass # Replace with function body.
-
-
 func _on_option_pressed():
-	show_and_hide(option,pause)
+	show_and_hide(option,stop)
 
 
 func _on_mainmenu_pressed():
@@ -47,7 +55,7 @@ func _on_audio_pressed():
 
 
 func _on_back_from_option_pressed():
-	show_and_hide(pause, option)
+	show_and_hide(stop, option)
 
 
 func _on_fullscreen_toggled(button_pressed):
@@ -91,3 +99,8 @@ func _on_sound_fx_value_changed(value):
 
 func _on_back_from_audio_pressed():
 	show_and_hide(option, audio)
+
+
+func _on_resume_pressed():
+	resume()
+	hide() 
