0fdc7802bb56c982561d05bf274d63a0a4f89745
Amir Hakimi <selamat.sejahtera.148@gmail.com>
2024-06-11 17:14:13 +0800

    fix

--- script/enemytest.gd
+++ script/enemytest.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 @onready var healthbar = $Healthbar
 @onready var coin_scene = preload("res://scenes/coins.tscn")
-@onready var item_scene = preload("res://scenes/item.tscn")
 var speed = 35
 var player_chase = false
 var player = null
@@ -14,7 +13,7 @@ var knockback_duration = 0.5
 var knockback_timer = 0.0
 var moving = false
 
-@export var item_drop_chance = 100
+
 
 func _ready():
 	$AnimatedSprite2D.play("default")
@@ -103,7 +102,6 @@ func take_damage(damage):
 		$AnimatedSprite2D.play("death")
 		await get_tree().create_timer(0.6).timeout
 		spawn_coins()
-		spawn_item()
 		queue_free()
 		
 	await get_tree().create_timer(6).timeout
@@ -133,11 +131,3 @@ func spawn_coins():
 		print("Coin spawned")
 
 
-
-
-func spawn_item():
-	if randi() % 100 < item_drop_chance:
-		var item_instance = item_scene.instantiate()
-		item_instance.position = position
-		get_parent().add_child(item_instance)
-		print("Item spawned at position: ", item_instance.position)
