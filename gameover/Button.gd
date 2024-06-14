e2e613a8bca7b05060f69836e7d0bf720abb3d4b
Hakimi <ammarbaharun@gmail.com>
2024-06-15 01:13:55 +0800

    victory_scene
    only scene, not yet linked

--- gameover/Button.gd
+++ gameover/Button.gd
@@ -2,18 +2,16 @@ extends Button
 
 func _ready():
 	var stylebox = StyleBoxFlat.new()
-	stylebox.bg_color = Color(0, 0, 1)  
+	stylebox.bg_color = Color(0,100,0)  
 	add_theme_stylebox_override("normal", stylebox)
 
 	var hover_stylebox = StyleBoxFlat.new()
-	hover_stylebox.bg_color = Color(0, 1, 0)  
+	hover_stylebox.bg_color = Color(10,128,0)  
 	add_theme_stylebox_override("hover", hover_stylebox)
 
 	var pressed_stylebox = StyleBoxFlat.new()
-	pressed_stylebox.bg_color = Color(1, 0, 0) 
+	pressed_stylebox.bg_color = Color(0,159,107) 
 	add_theme_stylebox_override("pressed", pressed_stylebox)
-
-
 
 
 func _on_pressed():
